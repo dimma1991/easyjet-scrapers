46136a7d5d2ed9b0d0ea436ef100b50a62b6b6aa
dimma1991 <dimitry@ug.edu.ge>
2025-06-09 12:26:22 +0400

    Update easyjetScraper.php

--- scraper/easyjetScraper.php
+++ scraper/easyjetScraper.php
@@ -1,7 +1,9 @@
 <?php
+
 function scrapeEasyJetPrice($origin, $destination, $date) {
-    $command = escapeshellcmd("node scraper_node.js "$origin" "$destination" "$date"");
-    $output = shell_exec($command);
+    $cmd = "node scraper_node.js " . escapeshellarg($origin) . " " . escapeshellarg($destination) . " " . escapeshellarg($date);
+    $output = shell_exec($cmd);
     return json_decode($output, true);
 }
+
 ?>
